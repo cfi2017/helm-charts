0c5daee611d3b081c17a7d4126388e60d0f94b8c
Carlo Field <carlo.field@adfinis.com>
2025-09-13 09:30:19 +0200

    fix: more rebranding

--- hack/ci/github/steps/update-page.sh
+++ hack/ci/github/steps/update-page.sh
@@ -17,19 +17,19 @@ HELMCHARTS_GOMPLATE_OUTPUT="$gh_pages_worktree/index.md" \
 HELMCHARTS_GOMPLATE_NAME=indexpage \
 hack/update-readme.sh
 
-cp --force adfinis-charts-repo.yaml "$gh_pages_worktree/adfinis-charts-repo.yaml"
+cp --force cfi2017-charts-repo.yaml "$gh_pages_worktree/cfi2017-charts-repo.yaml"
 
 mkdir --parent "$gh_pages_worktree/docs/images"
 cp --force docs/images/lunkwill_helm_shirt.png "$gh_pages_worktree/docs/images/lunkwill_helm_shirt.png"
 
 pushd "$gh_pages_worktree" > /dev/null
 
-git add index.md adfinis-charts-repo.yaml docs/images/lunkwill_helm_shirt.png
+git add index.md cfi2017-charts-repo.yaml docs/images/lunkwill_helm_shirt.png
 # abort pages update if no changes detected
 `git diff --cached --quiet` && exit 0
 git commit --message="Update site" --signoff
 
-readonly repo_url="https://x-access-token:$CR_TOKEN@github.com/adfinis/helm-charts"
+readonly repo_url="https://x-access-token:$CR_TOKEN@github.com/cfi2017/helm-charts"
 git push "$repo_url" gh-pages
 
 popd > /dev/null
